29ee9768f66e1f16958f2f16e8e28749c9957c7e
Joshua Bradt <bradt@nscl.msu.edu>
2014-12-17 17:31:08 -0500

    First commit: event file interface has been partially written, and a set of functions to plot on the pad plane has been made.

---
+++ evt.py
@@ -0,0 +1,334 @@
+import struct
+import numpy
+
+
+class EventFile:
+    """Represents a merged GET event file.
+
+    This class provides an interface to merged event files from the GET electronics. It is capable of opening
+    merged data files and reading events from them.
+    """
+
+    def __init__(self, filename=None):
+        """ Initializes and, optionally, opens an event file.
+
+        If a filename is provided, that file will be opened.
+
+        :param filename: The name of the file to open.
+        :return: None
+        """
+
+        self.magic = 0x6e7ef11e
+
+        if filename is not None:
+            self.open(filename)
+            self.is_open = True
+        else:
+            self.fp = None
+            self.is_open = False
+
+        return
+
+    def open(self, filename):
+        """ Opens the specified file.
+
+        The file's first 4 bytes are compared to the magic value ``0x6e7ef11e`` to check that the file is of the
+        correct type.
+
+        :param filename: The name of the file to open
+        :return: None
+        :except: IOError
+        """
+
+        try:
+            self.fp = open(filename, 'rb')
+            self.is_open = True
+            print("Opened file: " + filename)
+
+            read_magic = struct.unpack('<I', self.fp.read(4))[0]
+            if read_magic != self.magic:
+                print("Bad file. Magic number is wrong.")
+
+        except IOError:
+            raise
+
+        return
+
+    def close(self):
+        """Close an open file"""
+
+        if self.is_open:
+            self.fp.close()
+            self.is_open = False
+
+        return
+
+    def pack_sample(self, tb, val):
+
+        # val is 12-bits and tb is 9 bits. Fit this in 24 bits.
+        # Use one bit for parity
+
+        if val < 0:
+            parity = 1 << 12
+            narrowed = -val
+        elif val >= 4095:
+            parity = 0
+            narrowed = 4095
+        else:
+            parity = 0
+            narrowed = val
+
+        joined = (tb << 15) | narrowed | parity
+        return joined
+
+    def unpack_sample(self, packed):
+        """Unpacks a packed time bucket / sample pair.
+
+        :param packed: The packed value, as an integer
+        :return: A tuple containing (time bucket, sample)
+        """
+
+        tb = (packed & 0xFF8000) >> 15
+        sample = (packed & 0xFFF)
+        parity = (packed & 0x1000) >> 12
+
+        if parity == 1:
+            sample *= -1
+
+        return tb, sample
+
+    def unpack_samples(self, packed):
+
+        result = numpy.zeros(512)
+
+        tbs = (packed & 0xFF8000) >> 15
+        samples = (packed & 0xFFF)
+        parities = (packed & 0x1000) >> 12
+
+        # Change the parities from (0, 1, ...) to (1, -1, ...) and multiply
+        samples *= (-2 * parities + 1)
+
+        for x, y in zip(tbs, samples):
+            result[x] = y
+
+        return result
+
+    def read_next(self):
+        """Reads the next event from the file.
+
+        This function attempts to read an event beginning at the current file position. The position is checked by
+        checking the event magic number ``0xEE``.
+
+        :return: The event as an :class:`Event`
+        """
+
+        assert self.is_open
+
+        try:
+            # Read the event header
+            # This has the following structure:
+            #     (magic, size, ID, ts, nTraces)
+            hdr = struct.unpack('<BIIQH', self.fp.read(19))
+
+            if hdr[0] != 0xEE:
+                # This is not the beginning of the event. Seek back and fail.
+                self.fp.seek(-19, 1)
+                raise FilePosError(self.fp.name, "Event magic number was wrong.")
+
+            event_size = hdr[1]
+            event_id = hdr[2]
+            event_ts = hdr[3]
+            num_traces = hdr[4]
+
+            print("Found event of size", event_size, ":")
+            print("    Event Number:", event_id)
+            print("    Contains", num_traces, "traces")
+
+            new_evt = Event(evt_id=event_id, timestamp=event_ts)
+
+            # Read the traces
+
+            for n in range(num_traces):
+                # Read the trace header. The structure of this is:
+                #    (size, cobo, asad, aget, ch, pad)
+                th = struct.unpack('<IBBBBH', self.fp.read(10))
+
+                cobo = th[1]
+                asad = th[2]
+                aget = th[3]
+                ch = th[4]
+                pad = th[5]
+
+                # Now read the traces themselves
+                num_samples = (th[0] - 10) // 3  # (total - header) / size of packed item
+
+                for s in range(num_samples):
+                    packed = struct.unpack('<I', self.fp.read(3) + b'\x00')[0]
+                    unpacked = self.unpack_sample(packed)
+                    new_evt.traces[cobo, asad, aget, ch, unpacked[0]] = unpacked[1]
+
+            return new_evt
+
+        except Error as er:
+            raise er
+
+    def read_next_new(self):
+        """Reads the next event from the file.
+
+        This function attempts to read an event beginning at the current file position. The position is checked by
+        checking the event magic number ``0xEE``.
+
+        :return: The event as an :class:`Event`
+        """
+
+        assert self.is_open
+
+        try:
+            # Read the event header
+            # This has the following structure:
+            #     (magic, size, ID, ts, nTraces)
+            hdr = struct.unpack('<BIIQH', self.fp.read(19))
+
+            if hdr[0] != 0xEE:
+                # This is not the beginning of the event. Seek back and fail.
+                self.fp.seek(-19, 1)
+                raise FilePosError(self.fp.name, "Event magic number was wrong.")
+
+            event_size = hdr[1]
+            event_id = hdr[2]
+            event_ts = hdr[3]
+            num_traces = hdr[4]
+
+            print("Found event of size", event_size, ":")
+            print("    Event Number:", event_id)
+            print("    Contains", num_traces, "traces")
+
+            new_evt = Event(evt_id=event_id, timestamp=event_ts)
+
+            # Read the traces
+
+            for n in range(num_traces):
+                # Read the trace header. The structure of this is:
+                #    (size, cobo, asad, aget, ch, pad)
+                th = struct.unpack('<IBBBBH', self.fp.read(10))
+
+                cobo = th[1]
+                asad = th[2]
+                aget = th[3]
+                ch = th[4]
+                pad = th[5]
+
+                # Now read the trace itself
+                num_samples = (th[0] - 10) // 3  # (total - header) / size of packed item
+
+                raw_data_string = self.fp.read(3*num_samples)
+
+                packed = numpy.array([int.from_bytes(raw_data_string[n:n+3], 'little') for n in range(0, 3*num_samples, 3)])
+
+                unpacked = self.unpack_samples(packed)
+                new_evt.traces[cobo, asad, aget, ch] = unpacked
+
+            return new_evt
+
+        except Error as er:
+            raise er
+
+
+class Event:
+    """ Represents an event from an :class:`EventFile`.
+
+    The event is represented as a numpy array. The indices are:
+        (CoBo, AsAd, AGET, channel, time bucket)
+    """
+
+    def __init__(self, evt_id=0, timestamp=0):
+        """ Initialize a new event.
+
+        Attributes:
+            evt_id -- The event ID
+            timestamp -- The event's timestamp
+            traces -- A numpy array containing the event's data
+
+        :param evt_id: The event ID
+        :param timestamp: The event timestamp
+        :return: None
+        """
+        assert (evt_id >= 0)
+        assert (timestamp >= 0)
+
+        self.evt_id = evt_id
+        self.timestamp = timestamp
+        self.traces = numpy.zeros([10, 4, 4, 68, 512])
+
+        return
+
+
+    def hits(self, padmap):
+
+        hits_by_addr = self.traces.sum((4))
+        flat_hits = numpy.zeros(10240)
+
+        for addr, hitval in numpy.ndenumerate(hits_by_addr):
+            try:
+                flat_hits[padmap[addr]] = hitval
+            except KeyError:
+                pass
+
+        return flat_hits
+
+
+def load_pedestals(filename):
+    """ Loads pedestals from the provided file.
+
+    :param filename: The name of the file containing the pedestals.
+    :return: The pedestals as a numpy array
+    """
+
+    f = open(filename, 'r')
+    peds = numpy.zeros((10, 4, 4, 68, 512))
+
+    for line in f:
+        subs = line[:-1].split(',')  # throw out the newline and split at commas
+        vals = [int(float(x)) for x in subs]
+        peds[(vals[0], vals[1], vals[2], vals[3])] = vals[4] * numpy.ones(512)
+
+    return peds
+
+
+def load_padmap(filename):
+    """ Loads pad mapping from the provided file.
+
+    :param filename: The name of the file containing the pad mapping.
+    :return: The pad mapping as a dictionary {(cobo, asad, aget, ch): pad}
+    """
+
+    f = open(filename, 'r')
+    pm = {}
+
+    for line in f:
+        subs = line[:-1].split(',')  # throw out the newline and split at commas
+        vals = [int(float(x)) for x in subs]
+        pm[(vals[0], vals[1], vals[2], vals[3])] = vals[4]
+
+    return pm
+
+
+class Error(Exception):
+    """Base class for custom exceptions."""
+    pass
+
+
+class FilePosError(Error):
+    """Raised when data is read from a file whose read pointer does not seem
+    to be in the correct position.
+
+    Attributes:
+        filename -- the name of the file being read
+        details -- an explanation of the particular error
+        message -- an error message for printing
+    """
+
+    def __init__(self, filename, details=""):
+        self.filename = filename
+        self.details = details
+        self.message = "FilePosError in " + filename + ": " + details
---
+++ tpcplot.py
@@ -0,0 +1,151 @@
+import math
+import numpy
+import matplotlib.collections as coll
+import matplotlib.pyplot as plt
+import matplotlib.colors as mplcolors
+import matplotlib.cm as mplcm
+
+
+def create_triangle(x_offset, y_offset, side, orient):
+    """Generates a list of equilateral triangle vertices from the given parameters"""
+
+    return [[x_offset, y_offset],
+            [x_offset+side/2, y_offset+orient*side*math.sqrt(3.)/2.],
+            [x_offset+side, y_offset]]
+
+
+def generate_pad_plane():
+    """ Generates a list of the vertices of the pads on the pad plane.
+
+    :return: A numpy array of shape (10240, 3, 2)
+    """
+
+    small_z_spacing = 2*25.4/1000
+    small_tri_side = 184.*25.4/1000
+    umega_radius = 10826.772*25.4/1000
+    beam_image_radius = 4842.52*25.4/1000
+    pad_index = 0
+
+    # small_tri_hi = small_tri_side * math.sqrt(3.)/2.
+    small_x_spacing = 2 * small_z_spacing / math.sqrt(3)
+    small_y_spacing = small_x_spacing * math.sqrt(3)
+    dotted_s_tri_side = 4. * small_x_spacing + small_tri_side
+    dotted_s_tri_hi = dotted_s_tri_side * math.sqrt(3) / 2
+    dotted_l_tri_side = 2. * dotted_s_tri_side
+    dotted_l_tri_hi = dotted_l_tri_side * math.sqrt(3) / 2
+    large_x_spacing = small_x_spacing
+    large_y_spacing = small_y_spacing
+    large_tri_side = dotted_l_tri_side - 4*large_x_spacing
+    large_tri_hi = dotted_l_tri_side * math.sqrt(3) / 2
+    row_len_s = 2**math.ceil(math.log(beam_image_radius/dotted_s_tri_side) / math.log(2))
+    row_len_l = math.floor(umega_radius / dotted_l_tri_hi)
+    # hex_side = row_len_s * dotted_s_tri_side
+
+    # Some code was left out here from the original
+
+    xoff = 0
+    yoff = 0
+
+    # Create half a circle
+
+    pads = numpy.zeros((10240, 3, 2))
+
+    for j in range(row_len_l):
+        pads_in_half_hex = 0
+        pads_in_hex = 0
+        row_length = abs(math.sqrt(umega_radius**2 - (j*dotted_l_tri_hi + dotted_l_tri_hi/2)**2))
+
+        if j < row_len_s / 2:
+            pads_in_half_hex = (2 * row_len_s - 2*j)/4
+            pads_in_hex = 2*row_len_s - 1 - 2*j
+
+        pads_in_half_row = row_length / dotted_l_tri_side
+        pads_out_half_hex = int(round(2*(pads_in_half_row-pads_in_half_hex)))
+        pads_in_row = 2 * pads_out_half_hex + 4 * pads_in_half_hex - 1
+
+        ort = 1
+
+        for i in range(int(pads_in_row)):
+            if i == 0:
+                if j % 2 == 0:
+                    ort = -1
+                if ((pads_in_row - 1)/2) % 2 == 1:
+                    ort = -ort
+            else:
+                ort = -ort
+
+            pad_x_off = -(pads_in_half_hex + pads_out_half_hex/2) * dotted_l_tri_side \
+                + i * dotted_l_tri_side/2 + 2*large_x_spacing + xoff
+
+            if i < pads_out_half_hex or i > pads_in_hex + pads_out_half_hex - 1 or j > row_len_s / 2 - 1:
+                # Outside hex
+                pad_y_off = j*dotted_l_tri_hi + large_y_spacing + yoff
+                if ort == -1:
+                    pad_y_off += large_tri_hi
+                pads[pad_index] = create_triangle(pad_x_off, pad_y_off, large_tri_side, ort)
+                pad_index += 1
+            else:
+                # inside hex, make 4 small triangles
+                pad_y_off = j*dotted_l_tri_hi + large_y_spacing + yoff
+
+                # Omitted some code here
+
+                if ort == -1:
+                    pad_y_off = j*dotted_l_tri_hi + 2*dotted_s_tri_hi - small_y_spacing+yoff
+
+                pads[pad_index] = create_triangle(pad_x_off, pad_y_off, small_tri_side, ort)
+                pad_index += 1
+
+                tmp_pad_x_off = pad_x_off + dotted_s_tri_side/2
+                tmp_pad_y_off = pad_y_off + ort*dotted_s_tri_hi - 2*ort*small_y_spacing
+                pads[pad_index] = create_triangle(tmp_pad_x_off, tmp_pad_y_off, small_tri_side, -ort)
+                pad_index += 1
+
+                tmp_pad_y_off = pad_y_off+ort*dotted_s_tri_hi
+                pads[pad_index] = create_triangle(tmp_pad_x_off, tmp_pad_y_off, small_tri_side, ort)
+                pad_index += 1
+
+                tmp_pad_x_off = pad_x_off + dotted_s_tri_side
+                pads[pad_index] = create_triangle(tmp_pad_x_off, pad_y_off, small_tri_side, ort)
+                pad_index += 1
+
+    # Create symmetric pads
+    for i in range(pad_index):
+        pads[pad_index + i] = pads[i] * [1, -1]
+
+    return pads
+
+
+def show_pad_plane():
+    """Displays the pad plane"""
+
+    pts = generate_pad_plane()
+    c = coll.PolyCollection(pts)
+
+    fig, ax = plt.subplots()
+
+    ax.add_collection(c)
+    ax.autoscale_view()
+
+    fig.show()
+
+
+def pad_plot(data):
+
+    colors = mplcm.ScalarMappable().to_rgba(data)
+
+    pts = generate_pad_plane()
+    c = coll.PolyCollection(pts, facecolors=colors, edgecolors='face')
+
+    fig, ax = plt.subplots()
+
+    ax.add_collection(c)
+    ax.autoscale_view()
+
+    fig.show()
+
+
+
+
+if __name__ == "__main__":
+    generate_pad_plane()
