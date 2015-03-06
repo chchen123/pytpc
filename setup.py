449ab5b792dd5bd71720f0f29a9d9f4ba3beb0ae
Joshua Bradt <bradt@nscl.msu.edu>
2015-03-06 13:02:27 -0500

    Replaced distutils with setuptools.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='pytpc',
       version='0.1',
       description='Tools for analyzing TPC events in Python',
       author='Joshua Bradt',
       author_email='bradt@nscl.msu.edu',
-      modules=['pytpc/tracking.py', 'pytpc/simulation.py', 'pytpc/constants.py',
-               'pytpc/evtdata.py', 'pytpc/kalman.py', 'pytpc/tpcplot.py', 'pytpc/runtables.py'],
-      requires=['numpy', 'matplotlib', 'filterpy', 'scikit-learn', 'scipy', 'seaborn']
+      packages=['pytpc'],
+      install_requires=['numpy', 'matplotlib', 'filterpy', 'scipy', 'scikit-learn', 'seaborn'],
       )
