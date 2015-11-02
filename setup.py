5a510025f1abc36cfd639bc557823a74da723a24
Joshua Bradt <bradt@nscl.msu.edu>
2015-11-02 15:09:06 -0500

    Updated setup.py for new dependencies

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='pytpc',
-    version='0.5',
+    version='0.6',
     description='Tools for analyzing TPC events in Python',
     author='Joshua Bradt',
     author_email='bradt@nscl.msu.edu',
-    url = 'https://github.com/attpc/pytpc',
+    url='https://github.com/attpc/pytpc',
     packages=['pytpc'],
     install_requires=['scikit-learn',
                       'seaborn',
                       'matplotlib',
                       'scipy',
-                      'numpy'],
-    package_data = {'pytpc': ['data/gases/*', 'data/raw/*']},
+                      'numpy',
+                      'h5py'],
+    package_data={'pytpc': ['data/gases/*', 'data/raw/*']},
 
-    extras_require = {
+    extras_require={
         'docs': ['sphinx_bootstrap_theme>=0.4.5', 'sphinx>=1.2'],
     },
     )
