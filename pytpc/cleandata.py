54d5afb2d7e3b3cbe20e5392706ea280d3ce894f
Michelle Kuchera <kucheram@nscl.msu.edu>
2016-02-17 10:03:52 -0500

    initial commit for cleaning data

---
+++ pytpc/cleandata.py
@@ -0,0 +1,13 @@
+""" Cleaning data
+
+File : cleandata.py
+
+About : This module contains code to clean noise from spiral tracks in the data
+Author : M.P. Kuchera
+Created : 2/7/2016  Updated :  
+
+"""
+
+import h5py
+import numpy as np
+import pytpc.hdfdata as hdfdata
