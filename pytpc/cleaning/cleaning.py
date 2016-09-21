0010698aa5c1d74e1d7b3318a9312721a29fbaaf
Joshua Bradt <bradt@nscl.msu.edu>
2016-09-21 16:38:29 -0400

    Removed old functions, updated some to use new backend.

--- pytpc/cleaning/cleaning.py
+++ pytpc/cleaning/cleaning.py
@@ -128,48 +128,15 @@ class HoughCleaner(object):
         return np.column_stack((xyz, labels, mindists)), (cu, cv)
 
 
-def find_center_hough(xyzs):
-    xyz_order = xyzs[np.argsort(xyzs[:, 2])]
-
-    nbins = 200  # number of bins in r and theta for Hough space discretization
-    ths = np.linspace(0, pi, nbins)
-
-    npts = len(xyz_order) - 5
-    sqdels = xyz_order[5:, :2]**2 - xyz_order[:-5, :2]**2
-    deltas_tiled = np.tile(xyz_order[5:, :2] - xyz_order[:-5, :2], (nbins, 1))
-
-    Hrad = np.empty((nbins * npts, 2))
-
-    Hrad[:, 0] = np.repeat(ths, npts)
-    Hrad[:, 1] = (np.tile(np.sum(sqdels, -1), nbins) /
-                  (2 * (deltas_tiled[:, 0] * np.cos(Hrad[:, 0])
-                        + deltas_tiled[:, 1] * np.sin(Hrad[:, 0]))))
-
-    Hrad = Hrad[1:]
-
-    countsRad, xedgesRad, yedgesRad = np.histogram2d(Hrad[:, 0], Hrad[:, 1], nbins, range=((0, pi), (-500, 500)))
-
-    # max bin of histogram
-    iRad, jRad = np.unravel_index(countsRad.argmax(), countsRad.shape)
-    # convert max bin to theta, radius values
-    tRad = iRad * pi / nbins
-    rRad = jRad * 1000 / nbins - 500
-
-    # convert theta, r to postions in xy space
-    ax = rRad * np.cos(tRad)
-    by = rRad * np.sin(tRad)
-
-    return ax, by
-
-
 def nn_remove_noise(data, radius=40, num_neighbors=10):
     if isinstance(data, pd.DataFrame):
-        xyz = data.values
+        xyz = np.ascontiguousarray(data.values)
     else:
-        xyz = data
+        xyz = np.ascontiguousarray(data)
 
-    noise = np.sum((np.sqrt(np.sum((xyz[:, None, :3] - xyz[:, :3])**2, -1)) <= radius)
-                   & ~np.eye(len(xyz), dtype='bool'), axis=0) < num_neighbors
+    counts = nearest_neighbor_count(xyz, radius)
+
+    noise = counts < num_neighbors
 
     if isinstance(data, pd.DataFrame):
         return data.iloc[~noise]
