846eb009d36ba37ba4ebc5e2b6d612fbabd232fd
Joshua Bradt <bradt@nscl.msu.edu>
2016-12-29 09:53:18 -0500

    Fixed handling of case where pedestals=None.

--- pytpc/trigger/trigger.py
+++ pytpc/trigger/trigger.py
@@ -9,11 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 class TriggerSimulator(object):
-    def __init__(self, config, excluded_pads=[], pedestals=np.zeros(10240, dtype='float64')):
+    def __init__(self, config, excluded_pads=[], pedestals=None):
         self.padmap = read_lookup_table(config['padmap_path'])  # maps (cobo, asad, aget, ch) -> pad
         self.reverse_padmap = {v: k for k, v in self.padmap.items()}  # maps pad -> (cobo, asad, aget, ch)
         self.badpads = set(excluded_pads)
         logger.info('%d pads will be excluded from trigger', len(self.badpads))
+
+        if pedestals is None:
+            pedestals = np.zeros(10240, dtype='float64')
         self.pedestals = pedestals
 
         self.trigger = MultiplicityTrigger(config, self.reverse_padmap)
