934568c134a7677b8d1b6830b7c109ac8408f44d
Joshua Bradt <bradt@nscl.msu.edu>
2017-02-03 15:51:51 -0500

    Docs: wrote docstrings for hough wrapper.

--- pytpc/cleaning/hough_wrapper.pyx
+++ pytpc/cleaning/hough_wrapper.pyx
@@ -1,4 +1,8 @@
-# cython: profile=True
+"""hough_wrapper.pyx
+
+A Cython wrapper around `hough.c` that provides the Hough transform functions.
+
+"""
 
 import numpy as np
 cimport numpy as np
@@ -14,7 +18,25 @@ cdef extern from "hough.h":
 
 
 def hough_line(np.ndarray[np.double_t, ndim=2, mode='c'] xy, int nbins=500, int max_val=2000):
-    """ Finds lines in data
+    """Performs the linear Hough transform and returns the Hough space.
+
+    The generated Hough space has the angle along dimension 0 and the radius along dimension 1. The radii range
+    from `-max_val` to `max_val`. The angles range from 0 to pi.
+
+    Parameters
+    ----------
+    xy : ndarray
+        An array with the positional data to transform. The dimensions should be (N, 2), and the data must
+        have C-style row-major ordering.
+    nbins : int, optional
+        The number of bins to use in each dimension of the Hough space accumulator. The default value is 500.
+    max_val : int, optional
+        The extreme value in the Hough radius dimension. The default value is 2000.
+
+    Returns
+    -------
+    accum : ndarray
+        The Hough space accumulator, a 2D array with dimension `(nbins, nbins)`.
 
     """
     cdef np.ndarray[np.int64_t, ndim=2, mode='c'] accum = np.zeros((nbins, nbins), dtype=np.int64)
@@ -29,6 +51,23 @@ def hough_line(np.ndarray[np.double_t, ndim=2, mode='c'] xy, int nbins=500, int 
 
 
 def hough_circle(np.ndarray[np.double_t, ndim=2, mode='c'] xy, int nbins=200, int max_val=500):
+    """Performs the Hough transform for circles to find the center of a spiral.
+
+    Parameters
+    ----------
+    xy : ndarray
+        A 2D array of data with dimension `(N, 2)` and C-style row-major ordering.
+    nbins : int, optional
+        The number of bins to use in each dimension of the Hough space accumulator. The default value is 200.
+    max_val : int, optional
+        The extreme value in the Hough radius dimension. The default value is 500.
+
+    Returns
+    -------
+    cx, cy : float
+        The center of the spiral.
+
+    """
     cdef np.ndarray[np.int64_t, ndim=2, mode='c'] accum = np.zeros((nbins, nbins), dtype=np.int64)
     cdef double* xyPtr = <double*> xy.data
     cdef int64_t* accumPtr = <int64_t*> accum.data
@@ -51,6 +90,21 @@ def hough_circle(np.ndarray[np.double_t, ndim=2, mode='c'] xy, int nbins=200, in
 
 
 def nearest_neighbor_count(np.ndarray[np.double_t, ndim=2, mode='c'] xyz, double radius):
+    """Count the number of neighbors each point has within a given radius.
+
+    Parameters
+    ----------
+    xyz : ndarray
+        An Nx3 set of (x, y, z) data with C-style row-major ordering.
+    radius : float
+        The neighborhood radius.
+
+    Returns
+    -------
+    counts : ndarray
+        A one-dimensional array of length N containing the number of neighbors for each point in `xyz`.
+
+    """
     cdef np.ndarray[np.int64_t, ndim=1] counts = np.zeros(xyz.shape[0], dtype=np.int64)
 
     cdef double* xyzPtr = <double*> xyz.data
