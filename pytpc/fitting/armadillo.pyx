63a9d1357cc7d25c721e598b55f7e18627acebea
Joshua Bradt <bradt@nscl.msu.edu>
2017-02-17 14:09:00 -0500

    Docs: documented armadillo Cython wrapper.

--- pytpc/fitting/armadillo.pyx
+++ pytpc/fitting/armadillo.pyx
@@ -1,8 +1,17 @@
+"""armadillo.pyx
+
+This is a wrapper around the Armadillo C++ library. It provides functions to convert between
+NumPy and Armadillo objects. This is necessary since the C++ portion of the Monte Carlo code
+represents data using Armadillo vectors and matrices, while the Python part uses NumPy.
+
+"""
+
 cimport armadillo
 import numpy as np
 cimport numpy as np
 
 cdef mat * np2mat(np.ndarray[np.double_t, ndim=2] arr):
+    """Convert a 2D numpy array to an armadillo matrix."""
     cdef int i
     cdef int j
     cdef int dim0 = arr.shape[0]
@@ -18,6 +27,7 @@ cdef mat * np2mat(np.ndarray[np.double_t, ndim=2] arr):
     return armaArr
 
 cdef Mat[unsigned short] * np2uint16mat(np.ndarray[np.uint16_t, ndim=2] arr):
+    """Convert a 2D numpy array of uint16 to an armadillo matrix."""
     cdef int i
     cdef int j
     cdef int dim0 = arr.shape[0]
@@ -33,6 +43,7 @@ cdef Mat[unsigned short] * np2uint16mat(np.ndarray[np.uint16_t, ndim=2] arr):
     return armaArr
 
 cdef vec * np2vec(np.ndarray[np.double_t, ndim=1] arr):
+    """Convert a numpy vector to an armadillo vector."""
     cdef int i
     cdef int dim = arr.shape[0]
 
@@ -45,6 +56,7 @@ cdef vec * np2vec(np.ndarray[np.double_t, ndim=1] arr):
     return armaArr
 
 cdef np.ndarray[np.double_t, ndim=2] mat2np(const mat & armaArr):
+    """Convert an armadillo matrix to a 2D numpy array."""
     cdef np.ndarray[np.double_t, ndim=2] arr = np.empty((armaArr.n_rows, armaArr.n_cols), dtype=np.double, order='F')
 
     for i in range(armaArr.n_rows):
@@ -54,6 +66,7 @@ cdef np.ndarray[np.double_t, ndim=2] mat2np(const mat & armaArr):
     return arr
 
 cdef np.ndarray[np.double_t, ndim=1] vec2np(const vec & armaArr):
+    """Convert an armadillo vector to a numpy array."""
     cdef np.ndarray[np.double_t, ndim=1] arr = np.empty(armaArr.n_elem, dtype=np.double)
 
     for i in range(armaArr.n_elem):
