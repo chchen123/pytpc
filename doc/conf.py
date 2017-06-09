89e903f36881d58fbf8a077989ae7cdd6bc5f67d
Jack Taylor <jataylor@davidson.edu>
2017-06-09 13:15:11 -0400

    updated conf.py file

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+# Changed path from '..' to '../pytpc'
+sys.path.insert(0, os.path.abspath('../pytpc'))
 
 # -- General configuration ------------------------------------------------
 
