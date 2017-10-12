8809d26c8659a02cabe4735df732b6f0a4d647bf
Joshua Bradt <bradt@nscl.msu.edu>
2017-10-12 10:16:40 -0400

    Updated docs install script

--- doc/install_docs.sh
+++ doc/install_docs.sh
@@ -5,5 +5,5 @@ make html
 
 if [ $? == 0 ]; then
     cd _build/html
-    rsync -r --delete-before ./ attpc@fishtank.nscl.msu.edu:/soft/services/groups/attpc/doc/pytpc/
+    rsync -r --delete-before ./ fishtank.nscl.msu.edu:/soft/services/groups/attpc/doc/pytpc/
 fi
